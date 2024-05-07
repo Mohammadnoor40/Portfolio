2a3dfb28c71d5789a0c6c37d0761e62c89483245
Mohammadnoor40 <99839597+Mohammadnoor40@users.noreply.github.com>
2024-05-08 02:20:38 +0300

    Add files via upload

---
+++ Portfolio/vanilaJavaScript.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const accordionHeading = document.querySelector('.accordion-heading');
+    const skillsContent = document.getElementById('skills-content');
+    const skillsItems = skillsContent.querySelectorAll('h3');
+
+    let delay = 200; 
+
+    accordionHeading.addEventListener('click', function() {
+        accordionHeading.querySelector('i').classList.toggle('fa-plus');
+        accordionHeading.querySelector('i').classList.toggle('fa-minus');
+        skillsContent.classList.toggle('active');
+
+        skillsItems.forEach((item, index) => {
+            setTimeout(() => {
+                item.classList.toggle('visible');
+            }, delay * index); 
+        });
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const resumeLink = document.querySelector('.resume-link');
+    resumeLink.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        window.location.href = this.href; 
+    });
+});
